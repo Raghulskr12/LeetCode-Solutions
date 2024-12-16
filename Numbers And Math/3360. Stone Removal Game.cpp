f984a998f9e9a52dc770c088cda2b6245453b2aa
Raghulskr12 <kalairaghul70@gmail.com>
2024-12-16 15:07:52 +0530

    [ADD]New file has been added

---
+++ Numbers And Math/3360. Stone Removal Game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canAliceWin(int n) {
+        int turnCount = 0;  // Counter to track the number of turns
+        int decrement = 10;  // Start subtracting from 10 and decrease it over time
+
+        // Continue the game until the number becomes less than the current decrement value
+        while (n >= decrement) {
+            n -= decrement;  // Reduce the current number by the decrement value
+            decrement--;     // Decrease the decrement value for the next turn
+            turnCount++;     // Increment the turn count
+        }
+
+        // If the number of turns is even, Alice loses; if odd, Alice wins
+        return turnCount % 2 != 0;
+    }
+};
